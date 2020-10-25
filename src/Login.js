aec93db956caea97962e1b758e40389c113fb655
Shawal Kabir Chy <shawalkabirchy2020@gmail.com>
2020-10-25 20:12:30 +0600

    Initial Commit

---
+++ src/Login.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Login.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import { actionTypes } from "./reducer";
+
+function Login() {
+  const [{}, dispatch] = useStateValue();
+
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="http://pngimg.com/uploads/whatsapp/whatsapp_PNG12.png"
+          alt=""
+        />
+
+        <div className="login__text">
+          <h1>Sign in to WhatsApp</h1>
+        </div>
+
+        <Button type="submit" onClick={signIn}>
+          Sign In With Google
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
